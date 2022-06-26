6574a533dbaa5a1a2e72afd1e924f5cd15429bb8
Harkenware <106368226+Harkenware@users.noreply.github.com>
2022-06-26 03:56:29 -0400

    Add files via upload

--- module_6/Pytech_update.py
+++ module_6/Pytech_update.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
 db=MongoClient.documents
+db.collection.find({db.students})
+print("DISPLAYING STUDENTS DOCUMENTS FROM FIND()QUERY"+db.students)
 db.collection.updateOne(
    {"student_id":"1007"},
    {"$set":{"last_name":"daniels"}
    }
 )
+db.collection.find({"student_id":"1007"})
+print("DISPLAYING UPDATED STUDENT DOCUMENT FROM FIND_ONE()QUERY"+db.james)
